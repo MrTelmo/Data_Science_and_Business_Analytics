d5665e2e33d80f8365811a24c203dac37aff7633
TelmoC <telmoacarvalho@gmail.com>
2025-12-02 22:39:48 +0000

    Refactor comments and structure in data analysis script
    Updated comments for clarity and structure in the data analysis process. Refactored sections for better organization and readability.

--- class_5_python_basics.py
+++ class_5_python_basics.py
@@ -1,126 +1,106 @@
 import pandas as pd
 
-# Upload dataset
+# Upload Dataset
 vendas = pd.read_excel("/content/vendas_portugal.xlsx")
 vendas.head()
 
-# Brief analysis of the data set
+#1 Data Understanding
 vendas.info()
 vendas.describe()
 '''
 Null values:
 - x3 'cidade'
 - x5 'preco_unitario'; 'quantidade'; 'desconto_percent'
-
 Inconsistencies:
 - 'quantidade'; 'portes' as float instead of integer
 - 'cidade' and 'distrito' sometimes not matching
 - 'produto' and 'categoria' sometimes not matching
-
 Outliers:
-- na
+- NA
 '''
 
-# Dataset
-
-"""#Exercicio 3 - Preparação dos Dados"""
-
-#Trata NaN em preco_unitario e quantidade
-vendas["quantidade"]=vendas["quantidade"].fillna(vendas['valor_total']/vendas['preco_unitario']) # tirando partido dos valores de outras colunas e mantendo o float
-vendas["preco_unitario"]=vendas["preco_unitario"].fillna(vendas['valor_total']/vendas['quantidade']) # tirando partido dos valores de outras colunas e mantendo o float
-'''NOTA: opçao + correta seria criar novas colunas com os dados tratados em vez de alterar diretamente as colunas 'quantidade' e 'perco_unitario' '''
-#Corrige nomes
-vendas["cliente"]=vendas["cliente"].str.strip() # remover espaços no inicio/fim
-vendas["cliente"]=vendas["cliente"].str.title() # deixar no formato título
-#Corrige cidades
+#2 Brief Data Preparation
+  # Clean NaN on 'preco_unitario' and 'quantidade'
+vendas["quantidade"]=vendas["quantidade"].fillna(vendas['valor_total']/vendas['preco_unitario']) 
+vendas["preco_unitario"]=vendas["preco_unitario"].fillna(vendas['valor_total']/vendas['quantidade'])
+'''NOTE: It would be wiser/better to create new columns, transforming existing columns to fill the gaps
+and correct the dataset data quality '''
+  # Correct/transform client names - 'client'
+vendas["cliente"]=vendas["cliente"].str.strip()
+vendas["cliente"]=vendas["cliente"].str.title()
+  # Corret cities name - 'cidade'
 vendas["cidade"]=vendas["cidade"].str.strip() # remover espaços no inicio/fim
 vendas["cidade"]=vendas["cidade"].str.title() # deixar no formato título
+  # Check corrections
+vendas.info()
+vendas.head()
 
-vendas.info() # para testar/validar improvement
-vendas.head() # para testar/validar improvement
-
-#Calcula uma coluna receita = preco_unitario * quantidade * (1 - desconto_percent)
+# Identify sales aka 'receita' --> 'receita' = preco_unitario * unitario * (1-desconto_percent) 
 vendas["receita"] = vendas["preco_unitario"] * vendas["quantidade"] * (1 - vendas["desconto_percent"])
-#Converte data_venda para datetime
+# Convert 'data_venda' to datetime type
 vendas["data_venda"]= pd.to_datetime(vendas["data_venda"])
 vendas.head(10)
 
-"""#Exercicio 4 - Modelagem (Análise Exploratória)
-
-"""
-
-#Identificar quantos produtos são e distribuição de valores
+#3 Data Modelling
+  # Identify how many different products exist in the dataset and respective distribution
 produto_count=vendas['produto'].value_counts()
 print(produto_count)
-#Mostra o top 5 produtos em faturamento total
+
+  # Showcase top 5 products in sales
 vendas_top5 = vendas.groupby(["produto"])["receita"].sum().sort_values(ascending=False).reset_index()
 vendas_top5.head()
 
-#Cria um gráfico de barras com faturamento por canal de venda - receita por canal de vendas
+  # Plot a bar chart with all sales per sales channel - 'receita' per 'canal_venda'
 vendas_canal=vendas.groupby(["canal_venda"])["receita"].sum().reset_index().sort_values("receita", ascending=False)
-vendas_canal.head()
-
+vendas_canal.head() # just to check needed data
 vendas_canal.plot(title='Receita por canal de vendas')
 vendas_canal.plot.bar()
 
-#identificar quantas cidades e sua distribuição
+  # Identify how many different cities exist in the dataset and respective distribution
 cidade_count=vendas['cidade'].value_counts()
 print(cidade_count)
-#Calcula o ticket médio por cidade (receita média por venda)
+
+  # Identify the average amount per sale per city - aka mean 'receita' per 'cidade'
 vendas_cidade_average=vendas.groupby(["cidade"])["receita"].mean().sort_values(ascending=False).reset_index()
 vendas_cidade_average.round(2).head(7)
 
-"""#Exercicio 5 - Avaliação
-
-- Qual canal é mais lucrativo?  Online - ver venda_canal.plot em cima
-- Há diferenças entre cidades? - Sim, ver valores médios de receita em cada cidade min - Aveiro, max - Coimbra, ver desvios em baixo (.describe)
-- O desconto médio afeta a receita? Não, ver matroz de correlação em baixo
-
-"""
-
-#Há diferenças entre cidades?
+#4 Evaluation
+'''
+- Which channel is more profitable? Online - see sales_channel.plot (above)
+- Are there differences in revenue between cities? - Yes, see average revenue values ​​in each city (min - Aveiro, max - Coimbra), see deviations below with .describe() function
+- Does the average discount affect revenue? No, see the correlation matrix below
+'''
+  # Differences in revenue between cities
 vendas_cidade_average.describe()
 
-#O desconto medio afeta a receita?
-vendas['desconto_medio']=vendas['desconto_percent']/len(vendas['desconto_medio']) # calculo serie desconto medio == mean desconto_percent on vendas.describe()
-corr_matrix=vendas[['receita','desconto_medio']].corr() # calculo da matrix de correlação entre Receita e Desconto Medio
+  # Average discount affect revenue?
+vendas['desconto_medio']=vendas['desconto_percent']/len(vendas['desconto_medio']) # identify  average discount == mean 'desconto_percent' on vendas.describe()
+corr_matrix=vendas[['receita','desconto_medio']].corr() # correlation da matrix between 'receita' and 'deconto_medio'
 corr_matrix.head()
+'''Note: correlation == 0.075, so we can say there's no correlation'''
 
-# 0.075 é desprezivel assim não ha correlação
-
-"""# Exercicio 6 - Implantação"""
-
-#satisfacao_cliente é relativo a quê?! produto/cidade/canal venda ?!
-#avg_satisfaction cidade:
-  #avg_satisfaction=vendas.groupby('cidade')['avaliacao_cliente'].mean().sort_values(ascending=False)
-#avg_satisfaction canal venda:
-  #avg_satisfaction=vendas.groupby('canal_venda')['avaliacao_cliente'].mean().sort_values(ascending=False)
-#avg_satisfaction produto:
-  #avg_satisfaction=vendas.groupby('produto')['avaliacao_cliente'].mean().sort_values(ascending=False)
+#5 Deployment
+  # Create a dataframe summary entailing: city; most used channel; total revenue; and average client satisfaction
 
 from numpy._core.defchararray import count
-#Cria um resumo em DataFrame com: Cidade, Canal mais usado, Faturamento total e Avaliação média dos clientes
-#cidade -> already in vendas df
-#canal de venda mais usado por cidade (count por cidade)
-'''esta metrica tbm pode ser obtida atraves de .mode() -->
-  cidade_canal_vendas=vendas.groupby('cidade')['canal_venda'].apply(lambda x: x.mode()).reset_index().drop(columns=['level_1'])'''
+  # Most used sales channel per city (count # of sales per channel)
 cidade_canal_vendas=vendas.groupby('cidade')['canal_venda'].apply(lambda x: x.value_counts().idxmax()).reset_index()
 cidade_canal_vendas.head(7)
 
-#receita total por cidade (todos canais)
+  # Total revenue per city - total 'receita' per 'cidade' (all channels)
 cidade_receita=vendas.groupby('cidade')['receita'].sum().sort_values(ascending=False).reset_index()
 cidade_receita.head(7)
 
-#avaliação media dos clientes (por cidade?!)
+  # Average client satisfaction (per city)
 avg_city_satisfaction=vendas.groupby('cidade')['avaliacao_cliente'].mean().sort_values(ascending=False).reset_index()
 avg_city_satisfaction.head(7)
 
 vendas_resumo=pd.merge(cidade_canal_vendas,cidade_receita, how='inner', on='cidade')
 vendas_resumo.head(7)
 
-# Dataframe final
+  # Final Dataframe
 vendas_resumo_final=pd.merge(vendas_resumo,avg_city_satisfaction, how='inner', on='cidade')
 vendas_resumo_final.head(7)
 
-# Exportação do ficheito
+  # File export as .xlsx
 vendas_resumo_final.to_excel('resumo_vendas__final_portugal.xlsx', index=False)
