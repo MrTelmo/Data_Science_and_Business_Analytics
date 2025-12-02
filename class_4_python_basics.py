c5f720418c1f6b6f8c9c0061b92e93e99c0da420
TelmoC <telmoacarvalho@gmail.com>
2025-12-02 16:55:30 +0000

    Refactor functions and improve input prompts

--- class_4_python_basics.py
+++ class_4_python_basics.py
@@ -7,89 +7,74 @@ salutation(name)
 # Exercicio 2 - Create a function that sums numbers
 def sum_numbers(*args):
   return sum(args)
-
 number_1=int(input("Input number: "))
 number_2=int(input("Input another number: "))
 print("Sum: ", sum_numbers(number_1,number_2))
 
-#Exercicio 3 - TO BE CONTINUED
+#Exercicio 3 - Create a function that sums 3 different numbers and returns the average.
 def average(*args):
   return (sum(args)/len(args))
-
-number_1=int(input("Input number: "))
+number_1=int(input("Input a number: "))
 number_2=int(input("Input another number: "))
 number_3=int(input("Input last number: "))
 print("Average: ", average(number_1,number_2,number_3))
 
-#Exercicio 4
-def maior_numero(*args):
+# Exercicio 4 - Create a function that identifies the largest number.
+def max_number(*args):
   return max(args)
-
-number_1 = int(input("Input number: "))
+number_1 = int(input("Input a number: "))
 number_2 = int(input("Input another number: "))
 number_3 = int(input("Input last number: "))
-print("Maior numero: ", maior_numero(number_1,number_2,number_3))
+print("Max number: ", max_number(number_1,number_2,number_3))
 
-#Exercicio 5
-quadrado = lambda x: x*x
-number = int(input("Input number: "))
-print(f"Quadrado de {number} -> ", quadrado(number))
+# Exercicio 5 - Create a function that can calculate the square of a number.
+square = lambda x: x*x
+number = int(input("Input a number: "))
+print(f"The square of {number} -> ", square(number))
+#  or
+#  number = int(input("Input a number: "))
+#def square(number):
+#  return number*number
+#print(f"The square of {number} -> ", square(number))
 
-#  ou
-#  def quadrado(x):
-#    return x*x
-#  print(quadrado(3))
-
-#Exercicio 6
-#list_initial=[1,2,3,4,5]
-list_defined = list(range(int(input("Defina o valor máximo da lista: "))+1))
-print("Lista inicial -> ", list_defined)
+# Exercicio 6 - Create a function that generates a list of numbers from 0 to N, and then doubles the numbers in it in another list.
+list_defined = list(range(int(input("Define max value of the list: "))+1))
+print("List -> ", list_defined)
 list_doble = list(map(lambda x: x*2, list_defined))
-print("Lista dobrada -> ", list_doble)
+print("List Doubled -> ", list_doble)
 
-#Exercicio 7
-#list_numeros=[1,2,3,4,5,6,7,8]
-list_defined = list(range(int(input("Defina o valor máximo da lista: "))+1))
-pares_list=list(filter(lambda x: x % 2 == 0, list_defined))
-print(" # pares da lista -> ", pares_list)
+# Exercicio 7 - Create a function that identifies the even numbers of a list with max number N.
+list_defined = list(range(int(input("Define max value of the list: "))+1))
+even_list=list(filter(lambda x: x % 2 == 0, list_defined))
+print("Even #s of the list-> ", even_list)
 
-#Exercicio 8
-input_name=input("Introduzir nome: ")
-input_age=input("Introduzir idade: ")
-input_amount=input("Introduzir saldo: ")
+# Exercicio 8 - Create a function that generate a dictionary with client informations.
+input_name=input("Input name: ")
+input_age=input("Input age: ")
+input_amount=input("Input salary: ")
 def info_client(**kwargs):
   print(kwargs)
-info_client(nome=input_name, age=input_age, saldo=input_amount)
+info_client(name=input_name, age=input_age, salary=input_amount)
 
-#Exercicio 9
-def somar_todos(*args):
+# Exercicio 9 - Create a function that sum all numbers from 1 to N.
+def sum_all(*args):
   total=0
   for number in args:
     total += number
   return total
-
-list_numbers = list(range(1,int(input("Defina o numero máximo list: "))+1))
+list_numbers = list(range(1,int(input("Define max number of the list: "))+1))
 print("List numbers: ",list_numbers)
-print("Sum of list numbers: ", somar_todos(*list_numbers))
+print("Total sum of list numbers: ", sum_all(*list_numbers))
 
-#ou mais simples
-#print("Sum: ", somar_todos(1,3,6))
-
-#Exercicio 10
-def aplicar_imposto(valor, taxa=23):
-  imposto = round(valor*(1+(taxa/100)),2)
-  return imposto
-valor=float(input("Introduzir montante: "))
-taxa=(input("Introduzir taxa: "))
-if taxa == "":
-  print("Valor com taxa de 23% aplicada por defeito: ",aplicar_imposto(valor))
+# Exercicio 10 - Create a functinos that can calculate the price with VAT included. If no tax defined 23% applied by default, otherwise specified by the user.
+def vat(value, tax_value=23):
+  tax = round(value*(1+(tax_value/100)),2)
+  return tax
+value=float(input("Input amount: "))
+tax_value=(input("Input tax value: "))
+if tax_value == "":
+  print("Total amount with VAT included. Applied 23% by default: ",vat(value))
 else:
-  taxa=float(taxa)
-  print(f"Valor com taxa de {taxa}% aplicada: ",aplicar_imposto(valor,taxa))
+  tax_value=float(tax_value)
+  print(f"Total amount with VAT of {tax_value}% included: ",vat(value,tax_value))
 
-
-#ou mais simples
-#def aplicar_imposto(valor, taxa=23):
-#  imposto = round(valor*(1+(taxa/100)),2)
-#  return imposto
-#print("Valor com taxa: ",aplicar_imposto(100,0))
